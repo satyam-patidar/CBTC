68bd272b1e29b3fd4518e129257570b06cd9411b
iibrahim70 <aviknuhash70@gmail.com>
2024-02-06 20:01:18 +0600

    added hero section

---
+++ src/components/Hero.tsx
@@ -0,0 +1,80 @@
+import heroLeft from "../assets/images/hero-left.png";
+import linear from "../assets/images/linear.png";
+import heroRight from "../assets/images/hero-right.png";
+import checkMarkIcon from "../assets/icons/checkmark-icon.png";
+import { Button } from "./ui/button";
+
+const Hero = () => {
+  const eventDetails = [
+    {
+      id: 1,
+      title: "One day pass access all lecture",
+    },
+    {
+      id: 2,
+      title: "Lunch and Snack",
+    },
+    {
+      id: 3,
+      title: "Meet Event Speaker",
+    },
+    {
+      id: 4,
+      title: "Front Seat",
+    },
+  ];
+
+  return (
+    <section className="section-wrapper grid grid-cols-1 lg:grid-cols-3 gap-5">
+      {/* left side */}
+      <div className="lg:h-[calc(100dvh-80px)] lg:col-span-2 relative">
+        <img
+          src={heroLeft}
+          alt="LeftSide"
+          className="h-full w-full object-cover"
+        />
+
+        <img
+          src={linear}
+          alt="Linear Gradient"
+          className="absolute w-full bottom-0"
+        />
+
+        <div className="absolute bottom-5 lg:bottom-10 mx-5 lg:mx-[60px] space-y-3">
+          <h2>New Packages For Winter</h2>
+          <p>
+            Ut posuere felis arcu tellus tempus in in ultricies. Gravida id nibh
+            ornare viverra. <br /> Ultrices faucibus neque velit risus ac id
+            lorem. Always together.
+          </p>
+        </div>
+      </div>
+
+      {/* right side */}
+      <div className="lg:col-span-1 bg-black px-8 py-[30px] space-y-[30px]">
+        <h5 className="px-5">OUR MOST POPULAR SERVICES</h5>
+        <hr className="border-[#344E71]" />
+
+        <div className="px-5">
+          <img src={heroRight} alt="RightSide" className="object-cover mb-6" />
+          <h4>Corporate Event</h4>
+
+          {eventDetails.map((item) => (
+            <div key={item?.id}>
+              <div className="mt-4 flex items-center gap-3">
+                <img src={checkMarkIcon} alt="CheckMarkIcon" />
+                <p>{item?.title}</p>
+              </div>
+            </div>
+          ))}
+
+          <Button size="full" className="mt-[35px]">
+            View Details
+          </Button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
